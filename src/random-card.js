cf7813bed511f79bfa3c47655b731790bdc8fa20
CHOI DAE GEON <cdg@aplayz.co.kr>
2024-09-23 07:46:11 +0900

    랜덤카드

--- src/random-card.js
+++ src/random-card.js
@@ -11,16 +11,6 @@ var randomCard = function(){
  return randomValue() + '-' + randomSuit();
 };
 
-var randomHand = function(){
-  var cards = [];
-  cards.push(randomCard());
-  cards.push(randomCard());
-  cards.push(randomCard());
-  cards.push(randomCard());
-  cards.push(randomCard());
-  return cards;
-};
-console.log(randomHand());
 var buildCardArray = function(){
   var tempArray = [];
   for(var i=0; i < values.length; i++){
@@ -30,7 +20,34 @@ var buildCardArray = function(){
   };
   return tempArray;
 };
+var spliceCard = function(cardArray){
+  var takeAway = cardArray.splice(
+                 Math.floor(Math.random()*cardArray.length), 1)[0];
+  return [takeAway, cardArray];
+};
 
+
+var randomHand = function(){
+  var cards = [];
+  var cardArray = buildCardArray();
+  var result = spliceCard(cardArray);
+  cards[0] = result[0];
+  cardarray = result[1];
+  result = spliceCard(cardArray);
+  cards[1] = result[0];
+  cardarray = result[1];
+  result = spliceCard(cardArray);
+  cards[2] = result[0];
+  cardarray = result[1];
+  result = spliceCard(cardArray);
+  cards[3] = result[0];
+  cardarray = result[1];
+  result = spliceCard(cardArray);
+  cards[4] = result[0];
+  cardarray = result[1];
+  return cards;
+};
+console.log(randomHand());
 
 var wish = require('wish');
 var deepEqual = require('deep-equal');
@@ -68,5 +85,17 @@ describe('buildCardArray()', function() {
   });
 	it('does something?', function() {
     wish(deepEqual(buildCardArray(), ["1-H","1-D","1-S","1-C","2-H","2-D","2-S","2-C","3-H","3-D","3-S","3-C","4-H","4-D","4-S","4-C","5-H","5-D","5-S","5-C","6-H","6-D","6-S","6-C","7-H","7-D","7-S","7-C","8-H","8-D","8-S","8-C","9-H","9-D","9-S","9-C","10-H","10-D","10-S","10-C","J-H","J-D","J-S","J-C","Q-H","Q-D","Q-S","Q-C","K-H","K-D","K-S","K-C"]));
+  });
+});
+describe('spliceCard()', function() {
+  it('returns two things', function() {
+    wish(spliceCard(buildCardArray()).length === 2);
+  });
+  it('returns the selected card', function() {
+    wish(spliceCard(buildCardArray())[0].match(/\w{1,2}-[HDSC]/));
+  });
+  it('returns an array with one card gone', function() {
+    wish(spliceCard(buildCardArray())[1].length ===
+      buildCardArray().length - 1);
   });
 });
