3b8f5798c8b7ef0537fea7c6249fbc4b9adad153
CHOI DAE GEON <cdg@aplayz.co.kr>
2024-10-04 07:20:20 +0900

    change set

--- src/nb.js
+++ src/nb.js
@@ -1,7 +1,12 @@
-var easy = 'easy'
-var medium = 'medium'
-var hard = 'hard'
-// songs
+function fileName(){
+  var theError = new Error("here I am");
+  return theError.stack.match(/(\w+\.js)/)[1];
+};
+console.log(`Welcome to ${fileName()}!`);
+var easy = 'easy';
+var medium = 'medium';
+var hard = 'hard';
+
 imagine = ['c', 'cmaj7', 'f', 'am', 'dm', 'g', 'e7'];
 somewhereOverTheRainbow = ['c', 'em', 'f', 'g', 'am'];
 tooManyCooks = ['c', 'g', 'f'];
@@ -15,22 +20,19 @@ toxic = ['cm', 'eb', 'g', 'cdim', 'eb7', 'd7', 'db7', 'ab', 'gmaj7',
 bulletproof = ['d#m', 'g#', 'b', 'f#', 'g#m', 'c#'];
 
 var songs = [];
-var labels = [];
-var allChords = [];
-var labelCounts = [];
-var labelProbabilities = [];
+
+var allChords = new Set();
+var labelCounts = {};
+var labelProbabilities = {};
 var chordCountsInLabels = {};
 var probabilityOfChordsInLabels = {};
 
 function train(chords, label){
-  songs.push([label, chords]);
+	var labels = [];
+  songs.push({label, chords});
   labels.push(label);
-  for (var index = 0; index < chords.length; index++){
-    if(!allChords.includes(chords[index])){
-      allChords.push(chords[index]);
-    }
-  }
-  if((Object.keys(labelCounts).includes(label))){
+  chords.forEach(chord =>allChords.add(chord));
+  if(Object.keys(labelCounts).includes(label)){
     labelCounts[label] = labelCounts[label] + 1;
   } else {
     labelCounts[label] = 1;
@@ -45,15 +47,14 @@ function setLabelProbabilities(){
 
 function setChordCountsInLabels(){
   songs.forEach(function(song){
-    if(chordCountsInLabels[song[0]] === undefined){
-      chordCountsInLabels[song[0]] = {};
+    if(chordCountsInLabels[song.label] === undefined){
+      chordCountsInLabels[song.label] = {};
     }
-    song[1].forEach(function(chord){
-      if(chordCountsInLabels[song[0]][chord] > 0){
-        chordCountsInLabels[song[0]][chord] =
-chordCountsInLabels[song[0]][chord] + 1;
+    song.chords.forEach(function(chord){
+      if(chordCountsInLabels[song.label][chord] > 0){
+        chordCountsInLabels[song.label][chord] += 1;
       } else {
-        chordCountsInLabels[song[0]][chord] = 1;
+        chordCountsInLabels[song.label][chord] = 1;
       }
     });
   });
@@ -61,39 +62,40 @@ chordCountsInLabels[song[0]][chord] + 1;
 
 function setProbabilityOfChordsInLabels(){
   probabilityOfChordsInLabels = chordCountsInLabels;
-  Object.keys(probabilityOfChordsInLabels).forEach(function(difficulty){
-    Object.keys(probabilityOfChordsInLabels[difficulty]).forEach(function(chord){
+  Object.keys(probabilityOfChordsInLabels).forEach(
+function(difficulty){
+    Object.keys(probabilityOfChordsInLabels[difficulty]).forEach(
+function(chord){
       probabilityOfChordsInLabels[difficulty][chord] /= songs.length;
     });
   });
 }
 
-train(imagine, 'easy');
-train(somewhereOverTheRainbow, 'easy');
-train(tooManyCooks, 'easy');
-train(iWillFollowYouIntoTheDark, 'medium');
-train(babyOneMoreTime, 'medium');
-train(creep, 'medium');
-train(paperBag, 'hard');
-train(toxic, 'hard');
-train(bulletproof, 'hard');
+train(imagine, easy);
+train(somewhereOverTheRainbow, easy);
+train(tooManyCooks, easy);
+train(iWillFollowYouIntoTheDark, medium);
+train(babyOneMoreTime, medium);
+train(creep, medium);
+train(paperBag, hard);
+train(toxic, hard);
+train(bulletproof, hard);
 
 setLabelProbabilities();
 setChordCountsInLabels();
 setProbabilityOfChordsInLabels();
 
-
-
 function classify(chords){
-  var something = 1.01;
+  var smoothing = 1.01;
+  console.log(labelProbabilities);
   var classified = {};
   Object.keys(labelProbabilities).forEach(function(difficulty){
-    var first = labelProbabilities[difficulty] + something;
+    var first = labelProbabilities[difficulty] + smoothing;
     chords.forEach(function(chord){
       var probabilityOfChordInLabel =
 probabilityOfChordsInLabels[difficulty][chord];
       if(probabilityOfChordInLabel){
-        first = first * (probabilityOfChordInLabel + something);
+        first = first * (probabilityOfChordInLabel + smoothing);
       }
     });
     classified[difficulty] = first;
