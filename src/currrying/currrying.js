f508a567b6e614b45200c40a5bc221539c31976b
CHOI DAE GEON <cdg@aplayz.co.kr>
2024-12-11 07:56:48 +0900

    람다 적용

---
+++ src/currrying/currrying.js
@@ -0,0 +1,23 @@
+function add(numberOne, numberTwo) {
+  return numberOne + numberTwo;
+}
+
+function addFp(numberOne, numberTwo) {
+  return function (numberTwo) {
+    return numberOne + numberTwo;
+  };
+}
+
+R = require("ramda");
+
+const curriedAdd = R.curry(add);
+console.log(curriedAdd(1));
+
+console.log(curriedAdd(1)(2));
+console.log(curriedAdd(1, 2));
+
+const square = (thing) => thing * thing;
+console.log([2, 4, 5].map(square));
+
+const mapSquares = R.map(square);
+console.log(mapSquares([2, 4, 5]));
